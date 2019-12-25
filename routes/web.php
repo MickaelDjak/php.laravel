9562d1239a5ebe48e685c2cb2010d8801bafed17
mdjak <23djak@gmail.com>
2019-12-25 20:37:41 +0200

    Auth
    Generate auth contorller/view/routs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
-Route::get('/', 'HomeController@home')->name('home');
-Route::get('/contacts', 'HomeController@contact')->name('contacts');
+Route::get('/', 'IndexController@main')->name('main');
+Route::get('/contacts', 'IndexController@contact')->name('contacts');
 Route::resource('/posts', 'PostController');
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
